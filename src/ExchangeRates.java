7b96294f930982336a3a3ed48a57653fcfce04c4
SamFerguson <fergusonsd@g.cofc.edu>
2019-03-30 15:50:47 -0400

    made it so you can change array of currency codes and it compares the exchange rate between every node, probably going to want to make this its own method so it'll return something instead of logging

--- src/ExchangeRates.java
+++ src/ExchangeRates.java
@@ -11,47 +11,56 @@ public class ExchangeRates {
 
 
     private String currencyTag;
-
+    private static String[] currencies = {"usd", "jpy", "chf", "eur", "gbp", "aoa", "yer", "pyg"};
 
     public static void main(String[] args) throws Exception{
-        URL url = null;
-        try {
-            url = new URL("http://www.floatrates.com/daily/usd.json");
-        }catch(Exception e){
 
-        }
-
-        URLConnection con = url.openConnection();
-
-        InputStreamReader read = new InputStreamReader((con.getInputStream()));
-
-        BufferedReader read2 = new BufferedReader(read);
-
-        String rates = read2.readLine();
-
-        while(read2.readLine() != null){
-
-        }
-
-        read2.close();
-
-        if(rates != null && rates.length() >0 ){
-
-            JSONObject s = null;
-            try{
-                //get the big file
-                JSONObject temp = new JSONObject(rates);
-                //get the object ffrom that big file
-                s = temp.getJSONObject("eur");
-            }catch(Exception e){
+        for(String currency: currencies) {
+            System.out.println("this is the currency that is currently being compared    " + currency);
+            //make temporary currency array without the one you're currently on
+            String[] otherCurrencies = new String[currencies.length-1];
+            int index = 0;
+            for(int i = 0; i<currencies.length;i++){
+                //if the current currency in array isn't the one currently processed
+                //add it to the otherCurrencies array and incrememnt index
+                if(!currencies[i].equals(currency)){
+                    otherCurrencies[index] = currencies[i];
+                    index++;
+                }
             }
-            System.out.println(s.getFloat("rate"));
+            URL url = null;
+            try {
+                url = new URL("http://www.floatrates.com/daily/"+currency+".json");
+            } catch (Exception e) {
+            }
+            //open the connection
+            URLConnection con = url.openConnection();
+            //get input stream
+            InputStreamReader read = new InputStreamReader((con.getInputStream()));
+            //get a result from the stream
+            BufferedReader read2 = new BufferedReader(read);
 
-            System.out.println(rates);
+            //the first line of the stream ought to be the json from the webpage
+            String rates = read2.readLine();
+
+            while (read2.readLine() != null) {
+            }
+            read2.close();
+
+            //for each currency that you're using that isn't the one you're comparing to
+            for(String otherCurrency:otherCurrencies){
+                //if the json isn't empty
+                if (rates != null && rates.length() > 0) {
+                    //make the json object for the whole plaintext
+                    JSONObject tempJSON = new JSONObject(rates);
+                    //get the object from that file with that name
+                    //make this string variable against the other ones
+                    JSONObject s = tempJSON.getJSONObject(otherCurrency);
+                    System.out.print(s.getString("code")+":  ");
+                    System.out.println(s.getFloat("rate"));
+                }
+            }
         }
-
-
-
 
     }
 
